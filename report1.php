9c1eae0d0b12c950631728b12e270802835f88e0
Yaowaluck Promdee <ypromdee@gmail.com>
2016-08-09 14:52:23 +0700

    fix report1

--- report1.php
+++ report1.php
@@ -4,16 +4,102 @@
     <span class="title"><b id="title">ส่วนที่ 1 รายงานเฉพาะผู้ป่วยของ <ชื่อสถานบริการ></span>
     <span class="h5 date_start">Start Date: MM/DD/YYYY </span><span class="h5 date_end">End Date: MM/DD/YYYY</span></b><br>
     <h4>1.1 จำนวนผู้ป่วยประคับประคอง</h4>
-    <p>รูปที่ 1 ภาพรวมจำนวนผู้ป่วย ทั้งหมดใน Palliative Car Cloud ตั้งแต่เริ่ม กระทั่งปัจจุบัน</p>
-    <p>รูปที่ 2 จำนวนผู้ป่วย เฉพาะระยะที่เรียกแสดงรายงานคือระหว่าง <ว/ด/ป> ถึง <ว/ด/ป> </p>
-    <p>รูปที่ 3 จำนวนผู้ป่วย จำแนกตามรายปี ของ<ชื่อหน่วยบริการ>	</p>
-    <p>รูปที่ 4 จำนวนผู้ป่วย จำแนกตามรายเดือน ของ<ชื่อหน่วยบริการ> ปี พ.ศ.xxxx (n = xxxx)	</p>
-    <p>ตารางที่ 1 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามจังหวัดที่เป็นภูมิลำเนา	</p>
-    <h4>1.2 ลักษณะผู้ป่วยระยะประคับประคอง</h4>
-    <p>ตารางที่ 2 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามลักษณะประชากร นำเสนอเป็นค่าจำนวนและร้อยละ เว้นแต่จะระบุเป็นอย่างอื่นในบางรายการ</p>
-    <h4>1.3 ข้อมูลการรักษาระยะประคับประคอง</h4>
-    <p>ตารางที่ 3 ระยะเวลาการดูแลผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ></p>
-<p>ตารางที่ 4 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามข้อมูลการรักษาระยะประคับประคอง นำเสนอเป็นค่าจำนวนและร้อยละ เว้นแต่จะระบุเป็นอย่างอื่นในบางรายการ
-</p>
+    <div class="panel-group" id="accordion">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+            รูปที่ 1 ภาพรวมจำนวนผู้ป่วย ทั้งหมดใน Palliative Care Cloud ตั้งแต่เริ่ม กระทั่งปัจจุบัน</a>
+          </h4>
+        </div>
+        <div id="collapse1" class="panel-collapse">
+          <div class="panel-body">Graph </div>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+            รูปที่ 2 จำนวนผู้ป่วย เฉพาะระยะที่เรียกแสดงรายงานคือระหว่าง <ว/ด/ป> ถึง <ว/ด/ป> </a>
+          </h4>
+        </div>
+        <div id="collapse2" class="panel-collapse collapse">
+          <div class="panel-body">Graph</div>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+            รูปที่ 3 จำนวนผู้ป่วย จำแนกตามรายปี ของ<ชื่อหน่วยบริการ>	</a>
+          </h4>
+        </div>
+        <div id="collapse3" class="panel-collapse collapse">
+          <div class="panel-body"> Graph</div>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+              รูปที่ 4 จำนวนผู้ป่วย จำแนกตามรายเดือน ของ<ชื่อหน่วยบริการ> ปี พ.ศ.xxxx (n = xxxx)
+          </a>
+          </h4>
+        </div>
+        <div id="collapse4" class="panel-collapse collapse">
+          <div class="panel-body"> Graph</div>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+      ตารางที่ 1 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามจังหวัดที่เป็นภูมิลำเนา
+          </a>
+          </h4>
+        </div>
+        <div id="collapse5" class="panel-collapse collapse">
+          <div class="panel-body"> Graph</div>
+        </div>
+      </div>
+        <h4>1.2 ลักษณะผู้ป่วยระยะประคับประคอง</h4>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse6">
+    ตารางที่ 2 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามลักษณะประชากร นำเสนอเป็นค่าจำนวนและร้อยละ เว้นแต่จะระบุเป็นอย่างอื่นในบางรายการ
+          </h4>
+        </div>
+        <div id="collapse6" class="panel-collapse collapse">
+          <div class="panel-body"> Graph</div>
+        </div>
+      </div>
+
+<h4> 1.3 	ข้อมูลการรักษาระยะประคับประคอง</h4>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse7">
+      ตารางที่ 3 ระยะเวลาการดูแลผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ>
+        </div>
+        <div id="collapse7" class="panel-collapse collapse">
+          <div class="panel-body"> Graph</div>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse8">
+              ตารางที่ 4 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามข้อมูลการรักษาระยะประคับประคอง นำเสนอเป็นค่าจำนวนและร้อยละ เว้นแต่จะระบุเป็นอย่างอื่นในบางรายการ
+        </div>
+        <div id="collapse8" class="panel-collapse collapse">
+          <div class="panel-body"> Graph</div>
+        </div>
+      </div>
+
+
+
+
+    </div>
 </div>
 </div>
