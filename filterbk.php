860b473432f1e5ba39fb6f7fe6b1e85acf6b709b
Yaowaluck Promdee <ypromdee@gmail.com>
2016-08-09 12:13:40 +0700

    fix

--- filterbk.php
+++ filterbk.php
@@ -213,3 +213,38 @@
         </div><!--//end col-->
 
     </div>
+
+
+<!-- Report 1-->
+<!-- roport #1 -->
+<div class="panel panel-primary">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseThree">
+      ส่วนที่ 1 รายงานเฉพาะผู้ป่วยของ <ชื่อสถานบริการ></a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne" aria-expanded="false" style="height: 0px;">
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <span class="h4 date_start">Start Date: MM/DD/YYYY </span><span class="h4 date_end">End Date: MM/DD/YYYY</span><br>
+                    <h4>1.1 จำนวนผู้ป่วยประคับประคอง</h4>
+                    <p>รูปที่ 1 ภาพรวมจำนวนผู้ป่วย ทั้งหมดใน Palliative Car Cloud ตั้งแต่เริ่ม กระทั่งปัจจุบัน</p>
+                    <p>รูปที่ 2 จำนวนผู้ป่วย เฉพาะระยะที่เรียกแสดงรายงานคือระหว่าง <ว/ด/ป> ถึง <ว/ด/ป> </p>
+                    <p>รูปที่ 3 จำนวนผู้ป่วย จำแนกตามรายปี ของ<ชื่อหน่วยบริการ>	</p>
+                    <p>รูปที่ 4 จำนวนผู้ป่วย จำแนกตามรายเดือน ของ<ชื่อหน่วยบริการ> ปี พ.ศ.xxxx (n = xxxx)	</p>
+                    <p>ตารางที่ 1 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามจังหวัดที่เป็นภูมิลำเนา	</p>
+                    <h4>1.2 ลักษณะผู้ป่วยระยะประคับประคอง</h4>
+                  <p>ตารางที่ 2 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามลักษณะประชากร นำเสนอเป็นค่าจำนวนและร้อยละ เว้นแต่จะระบุเป็นอย่างอื่นในบางรายการ</p>
+                    <h4>1.3 ข้อมูลการรักษาระยะประคับประคอง</h4>
+                  <p>ตารางที่ 3 ระยะเวลาการดูแลผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ></p>
+<p>ตารางที่ 4 จำนวนและร้อยละผู้ป่วย ที่ให้บริการโดย<ชื่อหน่วยบริการ> จำแนกตามข้อมูลการรักษาระยะประคับประคอง นำเสนอเป็นค่าจำนวนและร้อยละ เว้นแต่จะระบุเป็นอย่างอื่นในบางรายการ
+</p>
+                </div>
+            </div>
+          </div>
+        </div>
+</div>
+
+<!--END -->
